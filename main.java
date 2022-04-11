8652c4307f21cf1c6d4875ce495090f1158b9165
julie2045 <julievo2045@gmail.com>
2022-04-11 10:02:36 -0400

    created the page that users will see. Might change the list of creatures to buttons instead.

---
+++ main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //displaying title and info about game 
+        System.out.println("Harry Potter Magical Creatures Battle!");
+        System.out.println("-".repeat(93));
+
+        System.out.println("Play against yourself/another person and choose the best mythical creature to win the battle! ");
+
+        System.out.println("Choose your magical creature to use for battle!");
+        System.out.println("-".repeat(93));
+        //list of creatures
+        System.out.println("Troll");
+        System.out.println("Dragon");
+        System.out.println("Hippogriff");
+        System.out.println("Unicorn");
+        System.out.println("Cerberus");
+        System.out.println("Mandrake");
+    }
+}
+
