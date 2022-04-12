6608668882c354de226b65457d4f0d3786fc7289
julie2045 <julievo2045@gmail.com>
2022-04-12 10:09:18 -0400

    added some features to two animals

---
+++ magical creatures/troll.java
@@ -0,0 +1,28 @@
+public class Troll {
+
+    public String species() {
+        return "Troll"; 
+    }
+
+    public String description () {
+        return "A large magical beast of prodigious strength and immense stupidity \nStrengths: strong and big \nWeaknesses: lack of intelligence"
+    }
+
+    public int health() {
+        return 100;
+    }
+
+    public int strength() {
+        return 15;
+    }
+
+    public String bestEnvir() {
+        return "Mountains";
+    }
+
+    //stock keeping unique
+    public String sku() {
+        return "tro0001";
+        
+    }
+}
