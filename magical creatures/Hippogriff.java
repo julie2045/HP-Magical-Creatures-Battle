ab46f075219e40958e1471ce365be61194e844be
julie2045 <julievo2045@gmail.com>
2022-04-17 21:40:40 -0400

    added more creatures

---
+++ magical creatures/Hippogriff.java
@@ -0,0 +1,30 @@
+public class Hippogriff extends Creature{
+
+    public String species() {
+        return "Hippogriff"; 
+    }
+
+    public String description () {
+        return " A magical beast that has the front legs, wings, and head of an eagle and the body, hind legs, and tail of a horse. Hippogriffs are carnivores and are extremely dangerous to be tamed. \nStrengths: quick reactions, loyal, independent \nWeaknesses: a bit too independent and have a hard time trusting others";
+    }
+
+
+    public int health() {
+        return 220;
+    }
+
+    public int strength() {
+        return 20;
+    }
+
+
+    public String bestEnvir() {
+        return "Forbidden Forest";
+    }
+
+    //stock keeping unit
+    public String sku() {
+        return "hip0001";
+
+    }
+}
---
+++ magical creatures/Unicorn.java
@@ -0,0 +1,30 @@
+public class Unicorn extends Creature{
+
+    public String species() {
+        return "Unicorn"; 
+    }
+
+    public String description () {
+        return "Magical pure-white horses with a single horn on their foreheads. \nStrengths: powerful healing properties, magic has no effect on them \nWeaknesses: afraid of loud noises, drawn towards those with pure of heart or spirit";
+    }
+
+
+    public int health() {
+        return 320;
+    }
+
+    public int strength() {
+        return 20;
+    }
+
+
+    public String bestEnvir() {
+        return "Forbidden Forest";
+    }
+
+    //stock keeping unit
+    public String sku() {
+        return "uni0001";
+
+    }
+}
